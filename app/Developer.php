86227fe11659c6b2a1b2369d982d9224037e1f60
Herbert <jbfuentebella25@gmail.com>
2019-08-19 11:08:02 -0400

    add model definitions

--- app/Developer.php
+++ app/Developer.php
@@ -3,8 +3,30 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Developer extends Model
 {
-    //
+    const TYPE_FRONTEND = 'frontend';
+    const TYPE_BACKEND = 'backend';
+
+    protected $fillable = ['name', 'type', 'slug'];
+
+    protected $attributes = [
+        'slug' => self::self::generateUniqueSlug(8),
+    ];
+
+    public static function generateUniqueSlug($tokenLength) 
+    {
+        do {
+            $newToken = Str::random($tokenLength);
+        } while (!empty(Developer::where('slug', $newToken)->first()));
+
+        return $newToken;
+    }
+
+    public function projects()
+    {
+        return $this->hasMany('App\Project');
+    }
 }
--- app/Project.php
+++ app/Project.php
@@ -3,8 +3,34 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Project extends Model
 {
-    //
+    protected $fillable = [
+        'name', 'client_name', 'lead_developer_id', 'slug'
+    ];
+
+    protected $attributes = [
+        'slug' => self::self::generateUniqueSlug(8),
+    ];
+
+    public static function findBySlug($slug)
+    {
+        return self::where('slug', '=', $slug)->first();
+    }
+
+    public static function generateUniqueSlug($tokenLength) 
+    {
+        do {
+            $newToken = Str::random($tokenLength);
+        } while (!empty(Project::where('slug', $newToken)->first()));
+
+        return $newToken;
+    }
+
+    public function leadDeveloper()
+    {
+        return $this->belongsTo('App\Developer');
+    }
 }
