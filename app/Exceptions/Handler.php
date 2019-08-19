d9e865555ab2e0fba450b48ed3ae60fd1916cdd1
Herbert <jbfuentebella25@gmail.com>
2019-08-19 14:07:33 -0400

    update to follow response format

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,9 @@ namespace App\Exceptions;
 
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
+
+use App\ResponseFormatter;
 
 class Handler extends ExceptionHandler
 {
@@ -47,5 +50,12 @@ class Handler extends ExceptionHandler
     public function render($request, Exception $exception)
     {
         return parent::render($request, $exception);
+    }
+
+    protected function invalidJson($request, ValidationException $exception)
+    {
+        $errors = ['errors' => $exception->errors()];
+
+        return ResponseFormatter::errorMsg($exception->getMessage(), $errors, $exception->status);
     }
 }
