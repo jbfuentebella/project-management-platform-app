31f18385f3c9201ac150ecad4b6b0cb5302542d5
Herbert <jbfuentebella25@gmail.com>
2019-08-19 11:04:44 -0400

    add request validation for saving new project

---
+++ app/Http/Requests/ProjectRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProjectRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required',
+            'client_name' => 'required',
+            'lead_developer_id' => 'required|integer|exists:developers,id',
+        ];
+    }
+
+    public function messages() 
+    {
+        return [
+            'required' => 'The :attribute cannot be blank.',
+            'exists' => 'Lead developer does\'nt exists.',
+            'integer' => 'Invalid parameter.'           
+        ];
+    }
+}
